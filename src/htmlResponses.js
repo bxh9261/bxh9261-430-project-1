02d37925e2d87ceae78af0bb5506de2650e72865
brad <bxh9261@g.rit.edu>
2021-02-24 15:08:47 -0500

    random joke node import

---
+++ src/htmlResponses.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const errorPage = fs.readFileSync(`${__dirname}/../client/error.html`);
+const aquaStyle = fs.readFileSync(`${__dirname}/../client/default-styles.css`);
+
+const get404Response = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(errorPage);
+  response.end();
+};
+
+const getCSSResponse = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(aquaStyle);
+  response.end();
+};
+
+module.exports.get404Response = get404Response;
+module.exports.getCSSResponse = getCSSResponse;
---
+++ src/index.js
@@ -0,0 +1,45 @@
+console.log('First web service starting up ...');
+
+// 1 - pull in the HTTP server module
+const http = require('http');
+
+// 2 - pull in URL and query modules (for URL parsing)
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+// 3 - locally this will be 3000, on Heroku it will be assigned
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/random-joke': jsonHandler.getRandomJokeResponse,
+  '/random-jokes': jsonHandler.getRandomJokeResponse,
+  '/default-styles.css': htmlHandler.getCSSResponse,
+  notFound: htmlHandler.get404Response,
+};
+
+// 7 - this is the function that will be called every time a client request comes in
+// this time we will look at the `pathname`, and send back the appropriate page
+// note that in this course we'll be using arrow functions 100% of the time in our server-side code
+const onRequest = (request, response) => {
+  let acceptedTypes = request.headers.accept && request.headers.accept.split(',');
+  acceptedTypes = acceptedTypes || [];
+
+  const parsedUrl = url.parse(request.url);
+  const { pathname } = parsedUrl;
+  const params = query.parse(parsedUrl.query);
+
+  const httpMethod = request.method;
+
+  if (urlStruct[pathname]) {
+    urlStruct[pathname](request, response, params, acceptedTypes, httpMethod);
+  } else {
+    urlStruct.notFound(request, response);
+  }
+};
+
+// 8 - create the server, hook up the request handling function, and start listening on `port`
+http.createServer(onRequest).listen(port);
+
+// console.log(`Listening on 127.0.0.1: ${port}`);
